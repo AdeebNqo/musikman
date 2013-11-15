7b32c96dbbe4c53aff62e51268a1d051cd2ecbf1
AdeebNqo <adeebnqo@gmail.com>
2013-11-15 16:19:02 +0200

    working on dir creation and file moving

--- util.py
+++ util.py
@@ -1,19 +1,27 @@
 import os
+import shutil
 class song(object):
 	def __init__(self, path):
-		f = open(path)
-		f.seek(-128,os.SEEK_END)
-		tag = f.read(3)
-		if (tag=="TAG"):
-			self.title = f.read(30)
-			self.artist = f.read(30)
-			self.album = f.read(30)
-			self.year = f.read(4)
-		else:
+		try:
+			f = open(path)
+			f.seek(-128,os.SEEK_END)
+			tag = f.read(3)
+			if (tag=="TAG"):
+				self.title = f.read(30)
+				self.artist = f.read(30)
+				self.album = f.read(30)
+				self.year = f.read(4)
+			else:
+				self.title = "Unknown"
+				self.artist = "Unknown"
+				self.album = "Unknown"
+				self.year = "Unknown"
+		except IOError:
 			self.title = "Unknown"
 			self.artist = "Unknown"
 			self.album = "Unknown"
 			self.year = "Unknown"
+			print("log: "+path)
 	def get_artist(self):
 		return self.artist
 	def get_album(self):
@@ -29,7 +37,7 @@ class filesystem(object):
 		for dirpath,dirnames,filenames in os.walk(directory):
 			self.folder_stack.append(dirpath)
 			for File in filenames:
-				self.file_stack.append(File)
+				self.file_stack.append(dirpath+"/"+File)
 	def nextfile(self):
 		self.file_index = self.file_index +1
 		if (self.file_index>=self.filestack_size):
@@ -41,3 +49,17 @@ class filesystem(object):
 			return False
 		else:
 			return True
+	def copy(self, srcpath, destpath, artist, album):
+		print("Copying...")
+		#creating directory that will contain file		
+		if (os.path.exists(destpath+"/"+artist)):
+			if (os.path.exists(destpath+"/"+artist+"/"+album)==False):
+				os.makedirs(destpath+"/"+artist+"/"+album)
+		else:
+			os.makedirs(destpath+"/"+artist+"/"+album)
+		#Copying file
+		shutil.copy(srcpath, destpath+"/"+artist+"/"+album)
+		print("copied!")
+				
+	def move(self, path, destpath, artist, album):
+		print("Moving...")
