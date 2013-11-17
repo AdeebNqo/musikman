0882a06bb761bec5d265e47f27186295303a112f
AdeebNqo <adeebnqo@gmail.com>
2013-11-17 11:24:30 +0200

    added method for quick logging

--- logger.py
+++ logger.py
@@ -11,12 +11,24 @@ class logger(object):
 	def __init__(self):
 		self.log_file="/tmp/.musikMan_log"
 		self.session_errors = []
+	#
+	# Method for logging sessions errors
+	#
 	def report_error(self, File, error):
 		issue = Issue(File,error)
 		self.session_errors.append(issue)
+	#
+	# Method for quick reporting of errors
+	# Must be use when there are a few errors
+	#
+	def qreport_error(self,File,error):
+		f = open(self.log_file,"w")
+		f.write(time.ctime()+": "+issue.File+", "+issue.error.__str__()+"\n")
+		f.write("\n")
+		f.close()	
 	def close(self):
 		f = open(self.log_file,"w")
 		for issue in self.session_errors:
-			f.write(time.ctime()+" File: "+issue.File+" Error: "+issue.error.__str__()+"\n")
+			f.write(time.ctime()+": "+issue.File+", "+issue.error.__str__()+"\n")
 		f.write("\n")
 		f.close()
