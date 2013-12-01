d4d8e092fa281f62ff87bcaa0b0ddb1773b44671
AdeebNqo <adeebnqo@gmail.com>
2013-12-01 19:34:25 +0200

    starting with progra opt not filesys manipulation

---
+++ 1.1/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+
+	Copyright 2013 Zola Mahlaza (AdeebNqo) <adeebnqo@gmail.com>
+
+
+*/
+#include<boost/program_options.hpp>
+int main(void){
+	namespace po = boost::program_options
+	/*
+
+	Adding the cmdline options
+
+	*/
+	return 0;
+}
