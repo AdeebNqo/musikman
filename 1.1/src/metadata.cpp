b5a1519b0735b861366005ad08a2e5254ae111a7
AdeebNqo <adeebnqo@gmail.com>
2014-01-04 13:04:15 +0200

    getting segfault when reading artist

--- 1.1/src/metadata.cpp
+++ 1.1/src/metadata.cpp
@@ -16,19 +16,43 @@ namespace music_man{
 		while(!infile->eof()){
 			infile->read(tmp,128);
 		}
-		std::reverse(&tmp[0], &tmp[127]);	
+		//std::reverse(&tmp[0], &tmp[127]);
+		int tag_pos = 0;		
+		for (int i=0; i<126; ++i){
+			char t = tmp[i];
+			char a = tmp[i+1];
+			char g = tmp[i+2];
+			if (t=='T' && a=='A' && g=='G'){
+				tag_pos = i+3;
+				break;
+			}
+		}
+		/*
 		
-		//reading the title
-		title = new char[30];
-		int j=0;
-		for (int i=3; i<34; ++i, ++j){
-			title[j] = tmp[34-i];
+		Try reading the rest of the tag
+		
+		*/
+		try{
+			//reading title
+			title = new char[30];
+			for (int index=0; index<30; ++index,++tag_pos){
+				title[index] = tmp[tag_pos];
+			}
+			//reading artist
+			artist = new char[30];
+			for (int index=0; index<30; ++tag_pos){
+				artist[index] = tmp[tag_pos];
+				std::cout << tmp[tag_pos];
+			}
+			std::cout << std::endl;
+		}catch(std::exception e){
+			std::cout << e.what() << std::endl;
 		}
 	};
 	metadata::~metadata(){
 		delete[] title;
 		delete[] artist;
-		delete[] album;
+		//delete[] album;*/
 		delete infile;
 	};
 	char* metadata::get_artist(){
