e5d103f61a6158a7acb1c76700f70ab84c2c7ea3
AdeebNqo <adeebnqo@gmail.com>
2014-06-28 05:21:08 +0200

    added direct folder managing

--- 1.1/main.py
+++ 1.1/main.py
@@ -10,6 +10,8 @@ import pyinotify
 import subprocess
 from mmutil import *
 import argparse
+from os.path import expanduser
+import os.path
 
 watchedfolders = []
 
@@ -36,7 +38,8 @@ class music_manager(daemon, pyinotify.ProcessEvent):
 	def process_IN_MOVED_TO(self,event):
 		self.place(event.pathname)
 	def ismusicfile(self, filepath):
-		response = subprocess.Popen("file "+filepath, stdout=PIPE).stdout.read()
+		p = subprocess.Popen(['file',filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		response, err = p.communicate()
 		if (response.split(':')[1].startswith(' Audio file ')):
 			return True
 		return False
@@ -54,22 +57,65 @@ class music_manager(daemon, pyinotify.ProcessEvent):
 				return False
 
 def main():
+
+	#reading saved folders which need to be watched
+	configpath = expanduser("~")+'/.musicman/config'
+	if not os.path.exists(os.path.dirname(configpath)):
+		os.makedirs(os.path.dirname(configpath))
+	if (not os.path.isfile(configpath)):
+		open(configpath,'a').close()
+	configfile = open(configpath,'r+')
+	for line in configfile.readlines():
+		watchedfolders.append(line)
+	#getting cmdline args
 	parser = argparse.ArgumentParser(description='Deamon for managing music.')
-	
-	daemon = music_manager('/tmp/music_manager.pid')
-	if len(sys.argv) == 2:
-	        if 'start' == sys.argv[1]:
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument('-f','--folder', nargs='+', help='Path of folder to watch for deamon to watch', required=False)
+	group.add_argument('-s','--status',  help='Control deamon. Start, stop or restart it', required=False)
+	group.add_argument('-p','--process',  help='Manage music in specified folder', required=False)
+	args = vars(parser.parse_args())
+	#processing provided folder
+	if (args['process']!=None):
+		folder = args['process']
+		#editing folder path incase it's a relative path
+		if (not os.path.isabs(folder)):
+			folder = os.path.abspath(folder)
+		tmp = music_manager('/tmp/tmp.pid') #deamon will not be started
+		print('processing...')
+		#iterating through all files in folder
+		for root, _, files in os.walk(folder):
+		    for f in files:
+			fullpath = os.path.join(root, f)
+			if tmp.ismusicfile(fullpath):
+				tmp.place(fullpath)
+		print('done')
+		
+	#processing new passed folders if there are any
+	if (args['folder']!=None):
+		newfolders = args['folder']
+		config = open(configpath)
+		for folder in newfolders:
+			#editing folder path incase it's a relative path
+			if (not os.path.isabs(folder)):
+				folder = os.path.abspath(folder)
+			watchedfolders.append(folder)
+			configfile.write(folder)
+			configfile.write('\n')
+	configfile.close()
+	#checking status of deamon if requested
+	if (args['status']!=None):
+		status = args['status'].lower();
+		daemon = music_manager('/tmp/music_manager.pid')
+	        if 'start' == status:
 	                daemon.start()
-	        elif 'stop' == sys.argv[1]:
+	        elif 'stop' == status:
 	                daemon.stop()
-	        elif 'restart' == sys.argv[1]:
+	        elif 'restart' == status:
 	                daemon.restart()
 	        else:
 	                print "Unknown command"
 	                sys.exit(2)
 	        sys.exit(0)
-	else:
-	        print "usage: %s start|stop|restart" % sys.argv[0]
-	        sys.exit(2)
+
 if __name__=='__main__':
 	main()
